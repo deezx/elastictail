0458ef7876564563ffd4a8d6e55dcbd683c84855
Diana Zvulun <deezx@users.noreply.github.com>
2017-10-01 22:25:41 +0300

    Create elastictail.py

---
+++ elastictail.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+try:
+    import re
+    import sys
+    import signal
+    import argparse
+    from time import sleep,time
+    from datetime import datetime
+    from pytz import timezone
+    from elasticsearch import Elasticsearch
+except Exception, e:
+    print('ERROR: import modules failed')
+    print('Exception %s' % e)
+    exit(1)
+
+try:
+    #Argument Parsing
+    parser = argparse.ArgumentParser(description='Command line tool for tailing logs from elasticsearch',formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-I', '-index', help='Index to query, defaults to _all', default='_all', dest='index', metavar='')
+    parser.add_argument('-S', '--size', help='Number of results to show, defaults to 1000', default=1000, dest='size', metavar='')
+    parser.add_argument('-H','--hostname', nargs='*', help='Hostname list', dest='hostname', metavar='HOST')
+    parser.add_argument('-T', '--type', help='Type of log to show, defaults to all', default='all', dest='type', metavar='')
+    parser.add_argument('-K', '--key', help='Key to search elasticsearch, this options should be entered with -V or --value argument', dest='key', metavar='')
+    parser.add_argument('-V', '--value', help='Value to search elasticsearch, this option should be entered with -K or --key argument', dest='value', metavar='')
+    args = parser.parse_args()
+
+    #Function that handles Ctrl+C
+    def signal_handler(signal, frame):
+        print('Ctrl+C pressed, exiting...')
+        sys.exit(0)
+
+    #Function that runs a search with specified parameters
+    #TODO: add scroll option to show all logs and not just es_size number of logs
+    def elasticsearch_query(es_client,es_index,es_size,query):
+        response = es_client.search(index=es_index,sort="@timestamp:asc",size=es_size, body=query)
+        return response
+
+    #Function that adds 'must' values to search query
+    def add_to_must_query(key,value):
+        search_query['query']['bool']['must'].append({"match_phrase": {key: value}},)
+
+    #Function that adds 'should' values to search query
+    def add_to_should_query(key,value):
+        search_query['query']['bool']['should'].append({"match": {key: value}},)
+
+    ### Main ###
+
+    #Check arguments
+    if (not args.hostname) and (not args.key and not args.value):
+        parser.print_help()
+        print("\nError: elastictail requires at least 1 argument: -H/--hostname or 2 arguments: -K/--key and -V/--value")
+        sys.exit(1)
+
+    #Ctrl+C handling
+    signal.signal(signal.SIGINT, signal_handler)
+
+    #Initialize elasticsearch
+    es_client = Elasticsearch(retry_on_timeout=True)
+
+    #Setting time for search, 20 seconds ago to now
+    time_now_millis = int(round(time() * 1000))
+    previous_time = time_now_millis-20000
+
+    #Build the query:
+    search_query =  {
+        "query": {
+            "bool": {
+                "must": [],
+                "minimum_should_match": 1,
+                "should": []
+            }
+        }
+    }
+
+    #Build the search query
+    #Check if hostname is a list or a string
+    if args.hostname:
+        if isinstance(args.hostname,list):
+            for hostname in args.hostname:
+                add_to_should_query('host.raw',hostname)
+        else:
+            add_to_should_query('host.raw',args.hostname)
+
+    #Check other arguments
+    if args.key and args.value:
+        add_to_must_query(args.key, args.value)
+
+    if args.type != 'all':
+        add_to_must_query("type", args.type)
+
+
+    #Add timestamp to search query
+    search_query['query']['bool']['must'].insert(0,{"range": {"@timestamp": {"gte": previous_time,"lte": time_now_millis,"format": "epoch_millis"}}})
+
+    while True:
+        #Run ES query with current values and search query
+        result=elasticsearch_query(es_client,args.index,args.size,search_query)
+        #print("Got %d Hits:" % result['hits']['total'])
+        for hit in result['hits']['hits']:
+            #Naive UTC object
+            datetime_obj = datetime.strptime(hit['_source']['@timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            #Convert to UTC
+            datetime_obj_utc = datetime_obj.replace(tzinfo=timezone('UTC'))
+            #Convert to EST
+            now_est = datetime_obj_utc.astimezone(timezone('America/New_York'))
+            #Convert to String
+            now_est_string = now_est.strftime("%d-%m-%Y %H:%M:%S.%f")[:-3]
+            #Handling Unicode messages and encoding it to utf-8
+            if not hit['_source']['message']:
+                message_final = ''
+            elif not isinstance(hit['_source']['message'],list):
+                message_final = hit['_source']['message'].encode("utf-8")
+            else:
+                message_final = hit['_source']['message']
+            if 'level' in hit['_source']:
+                if hit['_source']['level'].lower() == 'info':
+                    level_final = '\033[38;5;036m%s\033[0m' % hit['_source']['level']
+                elif hit['_source']['level'].lower() == 'warn':
+                    level_final = '\033[38;5;220m%s\033[0m' % hit['_source']['level']
+                elif hit['_source']['level'].lower() == 'error':
+                    level_final = '\033[38;5;196%s\033[0m' % hit['_source']['level']
+                elif hit['_source']['level'].lower() == 'debug':
+                    level_final = '\033[38;5;68m%s\033[0m' % hit['_source']['level']
+            else:
+                level_final = ''
+            print("{t} \033[38;5;104m{host}\033[0m {type} {l}: {m}".format(t=now_est_string, m=message_final, l=level_final, **hit["_source"]))
+
+
+
+        #Wait 20 seconds until next search
+        sleep(20)
+        previous_time = time_now_millis
+        time_now_millis = int(round(time() * 1000))
+        #Change timestamp
+        search_query['query']['bool']['must'][0]['range']['@timestamp']['gte'] = previous_time
+        search_query['query']['bool']['must'][0]['range']['@timestamp']['lte'] = time_now_millis
+
+except Exception:
+    print "Unexpected error:", sys.exc_info()
+    exit(1)
